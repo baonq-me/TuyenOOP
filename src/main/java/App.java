fec4587b8228b0a14a1f80eb18ef0e090011fd5a
baonq-me <quocbao747@gmail.com>
2021-11-22 18:38:56 +0700

    Update comments

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -23,6 +23,7 @@ class Tuyen implements DataProcessor {
 
 class Thang {
 
+    // Internal tool of Tiep. Nothing can touch
     private final DataProcessor dataProcessor;
 
     public Thang()
@@ -30,10 +31,12 @@ class Thang {
         dataProcessor = new Tuyen();
     }
 
+    // Give tool to someone if they ask
     public DataProcessor getDataProcessor() {
         return dataProcessor;
     }
 
+    // Take input to work. No one know about the tool
     public Integer processData(String data) {
         return dataProcessor.processData(data);
     }
